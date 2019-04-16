cf761db4df78691e78fb89fde715e5e562a32645
VerbalCPU <chalvi93@gmail.com>
2019-04-16 19:53:10 +0200

    install of firebase dependancy

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { Asset, Font } from 'expo';
-import * as firebase from 'firebase';
+import  * as firebase from 'firebase';
 
 
 export default class App extends React.Component {
