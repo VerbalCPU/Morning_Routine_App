2d5e726a2d7b5f8d84c46514129fee435916919f
VerbalCPU <chalvi93@gmail.com>
2019-04-21 01:14:47 +0200

    delete of faulty file

--- controllers/Login.js
+++ controllers/Login.js
@@ -1,5 +1,9 @@
 import fire_setup from './Firebase';
-
+import React,{Component} from 'react';
+import { StyleSheet, Text, View, TextInput} from 'react-native';
+import { Asset, Font } from 'expo';
+import {Container,Item,Form,Label,Input,Content,Header,Button} from "native-base";
+import { createAppContainer, createStackNavigator, StackActions, NavigationActions,StackNavigator } from 'react-navigation';
 
 const loginUser = (email, password) => {
 
@@ -9,6 +13,7 @@ const loginUser = (email, password) => {
       .then(res => {
         alert(res.user.email);
       });
+      this.props.navigation.navigate('./screens/Home');
 
   } catch (error) {
     console.log(error);
